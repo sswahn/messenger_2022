09096e54b0cb3e53731cfb773974d29bacd7bfa6
Steve Swahn <stephenaswahn@gmail.com>
2022-08-30 18:09:23 +0000

    Update UploadImageButton.js and button.module.css

--- src/components/buttons/UploadImageButton.js
+++ src/components/buttons/UploadImageButton.js
@@ -1,11 +1,24 @@
 import styles from './button.module.css'
 
 export default () => {
+  const handleClick = event => {
+    document.getElementById('file-upload').click()
+  }
+
+  const handleFileUpload = event => {
+    const file = event.target.files[0]
+    const data = new FormData()
+    data.append('file', file)
+    //Make a request to server and send formData
+    console.log('file:', file)
+    console.log('Input click fired: ', data)
+  }
 
   return (
-    <button type="button" className={styles.tooltip}>
+    <button type="button" className={styles.tooltip} onClick={handleClick}>
       <i className="fa fa-image"></i>
       <span className={styles.tooltiptext}>Upload</span>
+      <input id="file-upload" type="file" onChange={handleFileUpload} />
     </button>
   )
 }
